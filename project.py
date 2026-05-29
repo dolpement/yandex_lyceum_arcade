1a3508c864630aa561afd16d4ad91247e4170b2d
Полинуэль <polinuel@MacBook-Air-Polinuel.local>
2026-05-29 16:47:53 +0300

    GameView

--- project.py
+++ project.py
@@ -69,6 +69,100 @@ class StartView(arcade.View):
             self.window.show_view(game_view)
 
 
+class GameView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.player = None
+        self.player_list = None
+        self.item_list= None
+        self.enemy_list = None
+        self.score = 0
+        self.score_text = None
+        self.item_timer = 0
+        self.enemy_timer = 0
+        self.collect_sound = None
+        self.hit_sound = None
+
+    def setup(self):
+        arcade.set_background_color(arcade.color.BLACK)
+        self.player_list = arcade.SpriteList()
+        self.item_list = arcade.SpriteList()
+        self.enemy_list = arcade.SpriteList()
+        self.player = Player()
+        self.player_list.append(self.player)
+        self.score = 0
+        self.item_timer = 0
+        self.enemy_timer = 0
+        self.collect_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.hit_sound = arcade.load_sound(":resources:sounds/hit2.wav")
+
+    def on_draw(self):
+        self.cler()
+        self.player_list.draw()
+        self.item_list.draw()
+        self.enemy_list.draw()
+
+        arcade.draw_text(f"Score: {self.score}", 10, SCREEN_HEIGHT - 30, arcade.color.WHITE, 18)
+
+    def on_update(self, delta_time):
+        self.player.center_x += self.player.change_x * delta_time
+        if self.player.center_x < 20:
+            self.player.center_x = 20
+        elif self.player.center_x > SCREEN_WIDTH - 20:
+            self.player.center_x = SCREEN_WIDTH - 20
+
+        self.player.center_x += self.player.change_x * delta_time
+        if self.player.center_x < 20:
+            self.player.center_x = 20
+        elif self.player.center_x > SCREEN_WIDTH - 20:
+            self.player.center_x = SCREEN_WIDTH - 20
+
+        for item in self.item_list:
+            item.center_y += item.change_y * delta_time
+            if item.center_y < -20:
+                item.remove_from_sprite_lists()
+
+        for enemy in self.enemy_list:
+            enemy.center_y += enemy.change_y * delta_time
+            if enemy.center_y < -20:
+                enemy.remove_from_sprite_lists()
+
+        items_hit = arcade.check_for_collision_with_list(self.player, self.item_list)
+        for item in items_hit:
+            item.remove_from_sprite_lists()
+            self.score += 10
+            arcade.play_sound(self.collect_sound)
+
+        enemies_hit = arcade.check_for_collision_with_list(self.player, self.enemy_list)
+        if enemies_hit:
+            arcade.play_sound(self.hit_sound)
+            game_over = GameOverView(self.score)
+            self.window.show_view(game_over)
+            return
+
+        self.item_timer += delta_time
+        if self.item_timer >= ITEM_SPAWN_TIME:
+            self.item_timer = 0
+            item = Item()
+            self.item_list.append(item)
+
+        self.enemy_timer += delta_time
+        if self.enemy_timer >= ENEMY_SPAWN_TIME:
+            self.enemy_timer = 0
+            enemy = Enemy()
+            self.enemy_list.append(enemy)
+
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player.change_x = -PLAYER_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player.change_x = PLAYER_SPEED
+
+    def on_key_release(self, key, modifiers):
+        if key in (arcade.key.LEFT, arcade.key.A, arcade.key.RIGHT, arcade.key.D):
+            self.player.change_x = 0
+
+
 def main():
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     start_view = StartView()
